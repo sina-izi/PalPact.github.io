8c2fe50b6db4fb2d05406199ce204cca39bc6129
sina-izi <sinafallah98@gmail.com>
2024-12-11 01:06:07 -0500

    fix: changed globalstyle.css to App.css to keep the format globally

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import './styles/global.css'; // Optional: Add global styles
+import './App.css'; // Replace global.css with App.css
 
 ReactDOM.render(
   <Router>
